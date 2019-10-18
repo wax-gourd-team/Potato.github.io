0818920a0cc3571e90e6cdf0e90b13a03127666f
phh521034wyh <1159456617@qq.com>
2019-10-18 10:46:31 +0800

    ‘‘传

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './index';
+import App from './index.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
